eadc1087766cff700cd7db1edfb57094394a35d6
UserOrlov <orlvsrg@gmail.com>
2019-10-13 23:56:43 +0300

    My first comment in GIT

---
+++ src/main/java/TestGit/org/itstep/App.java
@@ -0,0 +1,13 @@
+package TestGit.org.itstep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Orlov in GIT" );
+    }
+}
